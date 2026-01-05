2f31ae3745e55c024e7981a5648138af9f67d459
Soaib Shaikh <samshaikh1192@gmail.com>
2026-01-05 16:53:17 +0530

    update

--- 02-redux-todos/src/App.jsx
+++ 02-redux-todos/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+
 import { useDispatch, useSelector } from 'react-redux'
 import { addUser, deleteUser } from './features/userSlice'
 
@@ -7,11 +7,7 @@ function App() {
   const { users } = useSelector(state => state.users)
   const dispatch = useDispatch()
 
-  const [form, setForm] = useState({
-    username: "",
-    email: "",
-    password: ""
-  })
+  const [form, setForm] = useState({})
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -27,56 +23,64 @@ function App() {
 
   return (
     <>
-      <form action="" method="post" onSubmit={handleSubmit}>
-        <h2>SignUp </h2>
-        <div className="mb-3">
-          <label htmlFor="username" className="form-label">Username</label>
-          <input type="text" name="username" value={form.username} onChange={handleChange} id="username" className="form-control" />
+      <div className="container">
+        <div className="row justify-content-center mt-5">
+          <div className="col-md-6">
+            <form action="" method="post" onSubmit={handleSubmit}>
+              <h2>SignUp </h2>
+              <div className="mb-3">
+                <label htmlFor="username" className="form-label">Username</label>
+                <input type="text" name="username" value={form.username || ''} onChange={handleChange} id="username" className="form-control" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email</label>
+                <input type="email" name="email" value={form.email || ''} onChange={handleChange} id="email" className="form-control" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" name="password" value={form.password || ''} onChange={handleChange} id="password" className="form-control" />
+              </div>
+              <button className="btn btn-outline-primary">Sign Up</button>
+            </form>
+          </div>
         </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email</label>
-          <input type="email" name="email" value={form.email} onChange={handleChange} id="email" className="form-control" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
-          <input type="password" name="password" value={form.password} onChange={handleChange} id="password" className="form-control" />
-        </div>
-        <button className="btn btn-outline-primary">Sign Up</button>
-      </form>
 
-      <table className='table table-bordered text-center table-responsive caption-top'>
-        <caption>
-          <h2>USers Data</h2>
+        <div className="row justify-content-center mt-5">
+          <table className='table table-bordered text-center table-responsive caption-top'>
+            <caption>
+              <h2>USers Data</h2>
 
-        </caption>
-        <thead>
-          <tr>
-            <th>Sr.no</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Password</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            users.map((val, index) => {
-              const { username, email, password, id } = val;
-              return (
-                <tr key={id}>
-                  <td>{index + 1}</td>
-                  <td>{username}</td>
-                  <td>{email}</td>
-                  <td>{password}</td>
-                  <td>
-                    <button type='button' className='btn btn-danger' onClick={() => dispatch(deleteUser(id))}>Delete</button>
-                  </td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
+            </caption>
+            <thead>
+              <tr>
+                <th>Sr.no</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                users.map((val, index) => {
+                  const { username, email, password, id } = val;
+                  return (
+                    <tr key={id}>
+                      <td>{index + 1}</td>
+                      <td>{username}</td>
+                      <td>{email}</td>
+                      <td>{password}</td>
+                      <td>
+                        <button type='button' className='btn btn-danger' onClick={() => dispatch(deleteUser(id))}>Delete</button>
+                      </td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   )
 }
